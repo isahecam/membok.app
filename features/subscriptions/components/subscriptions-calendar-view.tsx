212be388fc40f66e66ff9cfff874ceda8ad47693
Brandon Hernández <isahecam@gmail.com>
2026-05-13 14:37:38 -0600

    feat(subscription): enhance SubscriptionsCalendarView to display subscription avatars and counts

--- features/subscriptions/components/subscriptions-calendar-view.tsx
+++ features/subscriptions/components/subscriptions-calendar-view.tsx
@@ -1,20 +1,24 @@
 "use client";
 
+import { SERVICES_REGISTRY } from "@/features/subscriptions/lib/services-catalog";
 import { useWizardActions } from "@/features/subscriptions/store/subscription-wizard.store";
+import { Subscription } from "@/features/subscriptions/types/subscription.types";
+import { isSubscriptionDueOn } from "@/features/subscriptions/utils/is-subscription-due-on";
 import {
   CalendarCell,
   CalendarDaysWeek,
   CalendarGrid,
   CalendarMonthNavigator,
 } from "@/shared/components/blocks/calendar";
+import { Avatar, AvatarFallback, AvatarGroup, AvatarGroupCount, AvatarImage } from "@/shared/components/ui/avatar";
 import { Button } from "@/shared/components/ui/button";
 import { cn } from "@/shared/lib/utils";
 
-// interface Props {
-//   subscriptions: Subscription[];
-// }
+interface Props {
+  subscriptions: Subscription[];
+}
 
-export function SubscriptionsCalendarView(/* { subscriptions }: Props */) {
+export function SubscriptionsCalendarView({ subscriptions }: Props) {
   const { open } = useWizardActions();
 
   return (
@@ -23,14 +27,29 @@ export function SubscriptionsCalendarView(/* { subscriptions }: Props */) {
       <CalendarDaysWeek />
       <CalendarGrid>
         {(day) => {
-          // const subsForDay = subscriptions.filter((s) => dt.isSameDay(s.renewalDate, day.date));
+          const subsForDay = subscriptions.filter((s) => isSubscriptionDueOn(s, day.date));
           return (
             <Button
               variant="secondary"
               disabled={!day.isCurrentMonth}
               className={cn("size-full")}
               onClick={() => open(day.date)}>
-              <CalendarCell day={day}></CalendarCell>
+              <CalendarCell day={day}>
+                <AvatarGroup>
+                  {subsForDay.map((sub) => {
+                    const service = sub.service ? SERVICES_REGISTRY[sub.service] : null;
+                    if (!service) return null;
+
+                    return (
+                      <Avatar key={sub.id}>
+                        <AvatarImage src={service.logoUrl} alt={service.name} />
+                        <AvatarFallback>{service.name.charAt(0)}</AvatarFallback>
+                      </Avatar>
+                    );
+                  })}
+                  {subsForDay.length > 1 && <AvatarGroupCount>{subsForDay.length}</AvatarGroupCount>}
+                </AvatarGroup>
+              </CalendarCell>
             </Button>
           );
         }}
