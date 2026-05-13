d6dbd5cb50de1d84c45fae80b714919c2e7c1150
Brandon Hernández <isahecam@gmail.com>
2026-05-13 17:59:42 -0600

    feat(subscription): create edit subscription store for managing subscription edits

---
+++ features/subscriptions/store/edit-subscription.store.ts
@@ -0,0 +1,38 @@
+"use client";
+
+import { create } from "zustand";
+
+import type { Subscription } from "../types/subscription.types";
+import { useDaySubscriptionsActions } from "./day-subscriptions.store";
+
+type EditSubscriptionState = {
+  isOpen: boolean;
+  subscription: Subscription | null;
+  actions: {
+    open: (subscription: Subscription) => void;
+    close: () => void;
+  };
+};
+
+const initialState = {
+  isOpen: false,
+  subscription: null,
+};
+
+const useEditSubscriptionStore = create<EditSubscriptionState>((set) => ({
+  ...initialState,
+  actions: {
+    open: (subscription) => {
+      // Cierra el modal del día si estaba abierto
+      useDaySubscriptionsActions().close();
+
+      // Abre este modal con la suscripción a editar
+      set({ isOpen: true, subscription });
+    },
+    close: () => set(initialState),
+  },
+}));
+
+export const useEditSubscriptionIsOpen = () => useEditSubscriptionStore((s) => s.isOpen);
+export const useEditSubscriptionTarget = () => useEditSubscriptionStore((s) => s.subscription);
+export const useEditSubscriptionActions = () => useEditSubscriptionStore((s) => s.actions);
